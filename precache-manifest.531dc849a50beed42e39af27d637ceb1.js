0cc889eeb10acd12e65a22171e92dc90cbeb6f34
Mendy Joseph <info@vistateam.com>
2020-07-22 14:33:27 +0300

    Updates

---
+++ precache-manifest.531dc849a50beed42e39af27d637ceb1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681021b2bf183d6455297e7b65d0b322",
+    "url": "/Smart-Home/index.html"
+  },
+  {
+    "revision": "65cf18db69f07462fced",
+    "url": "/Smart-Home/static/css/2.71adfcb9.chunk.css"
+  },
+  {
+    "revision": "61ea0311bbccf18fa5c7",
+    "url": "/Smart-Home/static/css/main.a1caa301.chunk.css"
+  },
+  {
+    "revision": "65cf18db69f07462fced",
+    "url": "/Smart-Home/static/js/2.33d4dbad.chunk.js"
+  },
+  {
+    "revision": "e01755ceac4b87e9abc519b2042ab467",
+    "url": "/Smart-Home/static/js/2.33d4dbad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ea0311bbccf18fa5c7",
+    "url": "/Smart-Home/static/js/main.04388739.chunk.js"
+  },
+  {
+    "revision": "cb5cc0b212ac6daec4d6",
+    "url": "/Smart-Home/static/js/runtime-main.fbb02b86.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Smart-Home/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Smart-Home/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Smart-Home/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Smart-Home/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Smart-Home/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Smart-Home/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Smart-Home/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Smart-Home/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Smart-Home/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Smart-Home/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Smart-Home/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "caa919c8c9d7acdcf9fcdc6d49367233",
+    "url": "/Smart-Home/static/media/logo.caa919c8.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Smart-Home/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Smart-Home/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Smart-Home/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Smart-Home/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Smart-Home/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
